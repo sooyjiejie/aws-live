c5ab91d1cdd99de4366f7442c6ce1723d6df96ef
sooyjiejie <152348241+sooyjiejie@users.noreply.github.com>
2025-12-01 13:25:55 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cbsqukk4yuon.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "admin123"
-customdb = "employee"
-custombucket = "yongjie-employee"
-customregion = "us-east-1"
-
