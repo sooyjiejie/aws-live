266c38b74e7c9cc106d82e09be2747f90468116e
sooyjiejie <152348241+sooyjiejie@users.noreply.github.com>
2025-12-01 13:26:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cbsqukk4yuon.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "yongjie-employee"
+customregion = "us-east-1"
+
